f2a5aa3ea3026d11fe65ec4710bdadcb3b518f25
Patrik Ragan <ragan.patres@gmail.com>
2012-12-03 21:52:56 +0100

    added Comment model

---
+++ server/riddle/models/Comment.py
@@ -0,0 +1,13 @@
+from riddle import app, db, model_classes
+from peewee import *
+from riddle.models.Questionnaire import Questionnaire
+from flask_peewee.admin import ModelAdmin
+
+class Comment(db.Model):
+    response = TextField()
+    questionnaire = ForeignKeyField(Questionnaire)
+
+class CommentAdmin(ModelAdmin):
+    columns = ('response', 'questionnaire')
+
+model_classes.append((Comment, CommentAdmin))
