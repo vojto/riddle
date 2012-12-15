f1430aa81194d19b3972286bb4b0bd0de745ca4c
Ľubomír Remák <lubomirr@lubomirr.eu>
2012-12-15 14:08:29 +0100

    Add /get-ratings/.

--- server/riddle/views/common.py
+++ server/riddle/views/common.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, request, url_for, render_template
+from peewee import fn
 import qrcode
 import StringIO
+from riddle.views.helpers import *
 from riddle.models.Questionnaire import Questionnaire
+from riddle.models.Rating import Rating
 
 common = Blueprint('common', __name__, static_folder='../static', static_url_path='/static-test')
 
@@ -27,4 +30,23 @@ def qr_code(qaire_id):
 
     headers = {'Content-Type' : 'image/png'}
     return (imgdata, 200, headers);
+
+@common.route('/get-ratings/', methods=['POST'])
+def get_rating():
+    qaire_id = request.form['qaire_id']
+
+    qaires = Questionnaire.select().where(Questionnaire.id == qaire_id)
+    for qaire in qaires:
+        ratings = Rating.select(Rating, fn.Count(Rating.id).alias('count')).where(Rating.questionnaire == qaire).group_by(Rating.like)
+        ret = {'likes': 0, 'dislikes': 0}
+
+        for rating in ratings:
+            if rating.like:
+                ret['likes'] = rating.count
+            else:
+                ret['dislikes'] = rating.count
+
+        return json.dumps(ret)
+
+    return response_error('questionnaire_not_found')
 
