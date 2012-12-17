d64f12861713d1ccdadedc94ec9dbc290d8f791b
Vojto Rinik <vojto@rinik.net>
2012-12-17 22:04:55 +0100

    Copying index.html in build

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+cp client/public/index.html server/riddle/templates/index.html
 cd client
 hem build
 echo 'window.isProduction=true; ' | cat - public/application.js > /tmp/out && mv /tmp/out public/application.js
