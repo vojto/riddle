f2ca52fb026b8ec4deb2c088843b71d8398c8ec8
Vojto Rinik <vojto@rinik.net>
2012-12-17 20:39:50 +0100

    Excluding data.db

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ./build.sh
-rsync -a . vojto@rinik.net:/var/www/apps/riddle
+rsync -a --exclude="data.db" . vojto@rinik.net:/var/www/apps/riddle
 ssh vojto@rinik.net "cd /var/www/apps/riddle/server && forever stop -c python run.py && forever start -c python run.py"
